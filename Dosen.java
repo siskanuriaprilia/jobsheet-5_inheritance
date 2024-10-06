fc849ac0f1969f217fcaccb3b3ebbdf2769a5d20
siskanuriaprilia <aprilias878@gmail.com>
2024-10-06 21:42:05 +0700

    percobaan2

--- Dosen.java
+++ Dosen.java
@@ -1,4 +1,6 @@
 public class Dosen extends Pegawai {
+        public String nidn;
+        
         public Dosen () {
             System.out.println("Objek dari class Dosen dibuat");
         }
--- InheritanceDemo.java
+++ InheritanceDemo.java
@@ -1,5 +1,12 @@
 public class InheritanceDemo {
     public static void main(String[] args) {
         Dosen dosen1 = new Dosen();
+
+        dosen1.nama = "Yansy Ayuningtyas";
+        dosen1.nip = "34329837";
+        dosen1.gaji = 3000000;
+        dosen1.nidn = "19894323439";
+
+        System.out.println(dosen1.getInfo());
     }
 }
