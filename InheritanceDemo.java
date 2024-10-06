6e3af1b05dc9dbff6e19d9eaad27c508c4487b4a
siskanuriaprilia <aprilias878@gmail.com>
2024-10-06 21:30:21 +0700

    percobaan 1

---
+++ InheritanceDemo.java
@@ -0,0 +1,5 @@
+public class InheritanceDemo {
+    public static void main(String[] args) {
+        Dosen dosen1 = new Dosen();
+    }
+}
---
+++ Pegawai.java
@@ -0,0 +1,6 @@
+public class Pegawai {
+
+    public Pegawai () {
+        System.out.println("Objek dari class Pegawai dibuat");
+    }
+}
